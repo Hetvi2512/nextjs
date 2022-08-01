73755fb2ca5def8c519d37c2b83a449d1137b4c5
hetvi2512 <hetvishah2512@gmail.com>
2022-08-01 14:14:02 +0530

    Added Search Icon

---
+++ components/common/Navbar/Search.jsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import {useState} from "react"
+import SearchIcon from "../../../public/images/search.svg";
+// import ModernDrawer from "./ModernDrawer.js";
+
+function Search() {
+  const [searchVisible, setSearchVisible] = useState(false);
+  const onShowDrawerSearch = () => {
+    setSearchVisible(true);
+  };
+  const onCloseSearch = () => {
+    setSearchVisible(false);
+  };
+  return (
+    <div className="search-div">
+      <Image
+        onClick={onShowDrawerSearch}
+        className="cursor-pointer"
+        src={SearchIcon}
+        alt="search-icon"
+      />
+        <div className="search-bar-drawer">
+        {/* <ModernDrawer searchVisible={searchVisible} onCloseSearch={onCloseSearch} /> */}
+          </div>
+    </div>
+  );
+}
+export default Search;
+ 
