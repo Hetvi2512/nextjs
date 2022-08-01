b2a72f03a95a78d9758771c4970f2dd2da3edbb8
hetvi2512 <hetvishah2512@gmail.com>
2022-08-01 19:20:48 +0530

    Added styles

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,12 @@ import '../styles/scss/helper.scss'
 import '../styles/scss/app.scss'
 import '../styles/globals.css'
 import '../styles/app.css'
+import '../styles/Navbar.css'
+import '../styles/StorySection.css'
+import '../styles/Ticker.css'
+import '../styles/ArticleLayout.css'
+import '../styles/StyleSection.css'
+import "react-modern-drawer/dist/index.css";
 
 
 function MyApp({ Component, pageProps }) {
