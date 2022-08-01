ffaeafafc1bcd2219bf5d804330d5f6f461cff80
hetvi2512 <hetvishah2512@gmail.com>
2022-08-01 14:14:39 +0530

    Added css imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
+import "antd/dist/antd.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/scss/helper.scss'
+import '../styles/scss/app.scss'
 import '../styles/globals.css'
+import '../styles/app.css'
+
 
 function MyApp({ Component, pageProps }) {
+
   return <Component {...pageProps} />
 }
 
