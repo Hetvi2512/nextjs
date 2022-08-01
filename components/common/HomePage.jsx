f77dc01f627011bb66959978970d8b5dd8e89cd4
hetvi2512 <hetvishah2512@gmail.com>
2022-08-01 14:08:03 +0530

    Added HomePage

---
+++ components/common/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header/Header1";
+
+function HomePage(props) {
+  return (
+    <>
+      <Header />
+      <div style={{ height: "500px" }}>
+        <h3>HEIGHT</h3>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
