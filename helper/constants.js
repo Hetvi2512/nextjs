8da8d7c11bb5b2629f585391702e3af716f3a87a
hetvi2512 <hetvishah2512@gmail.com>
2022-08-01 14:09:27 +0530

    Added constants

---
+++ helper/constants.js
@@ -0,0 +1,93 @@
+export const baseUrl = 'https://theestablished.qtstage.io';
+// export const imgBaseURL = 'https://gumlet.assettype.com/';
+export const imgBaseURL = 'https://gumlet.assettype.com/';
+export const gaTag = 'GTM-WWGMNM2';
+
+export const categoryID={
+    styleID:'38240',
+    fashionID:'38242',
+    sneakersID:'38244',
+    watchesID:'38245',
+    selfID:'38237',
+    beautyID:'38238',
+    healthID:'38239',
+    communityID:'38226',
+    consciousLivingID:'38228',
+    businessID:'38227',
+    identityID:'38229',
+    viewpointID:'38230',
+    cultureID:'38231',
+    autoID:'38232',
+    entertainmentID:'38233',
+    gamingID:'38234',
+    livingID:'38235',
+}
+export const subCatPageUrl={
+    'fashion':{
+        url:'/style',
+        key:'1'
+    },
+    'sneakers':{
+        url:'/style',
+        key:'2'
+    },
+    'watches':{
+        url:'/style',
+        key:'3'
+    },
+    'beauty':{
+        url:'/self',
+        key:'1'
+    },
+    'health':{
+        url:'/self',
+        key:'2'
+    },
+    'conscious-living':{
+        url:'/community',
+        key:'2'
+    },
+    'business':{
+        url:'/community',
+        key:'1'
+    },
+    'identity':{
+        url:'/community',
+        key:'3'
+    },
+    'viewpoint':{
+        url:'/community',
+        key:'4'
+    },
+    'living':{
+        url:'/culture',
+        key:'4'
+    },
+    'culture':{
+        url:'/culture',
+        key:'0'
+    },
+    'auto':{
+        url:'/culture',
+        key:'1'
+    },
+    'gaming':{
+        url:'/culture',
+        key:'3'
+    },
+    'entertainment':{
+        url:'/culture',
+        key:'2'
+    },
+    'style':{
+        url:'/style',
+        key:'0'
+    },'self':{
+        url:'/self',
+        key:'0'
+    },'community':{
+        url:'/community',
+        key:'0'
+    }
+}
+
