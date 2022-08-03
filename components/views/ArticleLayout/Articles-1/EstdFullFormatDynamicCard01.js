d207428714dca5f210a3b89801879e1bbe0289ab
hetvi2512 <hetvishah2512@gmail.com>
2022-08-03 11:28:21 +0530

    Added Full formate dynamic card 01

---
+++ components/views/ArticleLayout/Articles-1/EstdFullFormatDynamicCard01.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import SocialMediaShareButton from '../../SocialMediaShareButton';
+
+function EstdFullFormatDynamicCard01({ heroData, seoData, cards }) {
+    const storyElement = cards['story-elements']
+
+    return (
+        <div className="ptp-5 pbp-5">
+        <div className="row ma-0 row-data">
+          {storyElement.map((element) =>
+            element.type === "text" ? (
+              <div className="col-lg-6 col-xl-6 col-md-12 col-sm-12 p-0">
+                <div className="ffd-social-ic">
+                  <div className="d-flex main-social-icon">
+                    <SocialMediaShareButton title={heroData?.title} description={seoData?.description}/>
+                  </div>
+                </div>
+                <div
+                  className="description-head"
+                  dangerouslySetInnerHTML={{ __html: element.text }}
+                ></div>
+              </div>
+            ) : element.type === "image" ? (
+              <div className="col-lg-6 col-xl-6 col-md-12 col-sm-12 p-0">
+                <div className="ffd-b-img">
+                  <img
+                    width="100%"
+                    src={`https://gumlet.assettype.com/${element["image-s3-key"]}`}
+                    alt="fashion"
+                  ></img>
+                  <div
+                    className="HeldaneDisplay-Regular image-text"
+                    dangerouslySetInnerHTML={{ __html: element["title"] }}
+                  ></div>
+                </div>
+              </div>
+            ) : (
+              ""
+            )
+          )}
+
+          {/* {
+                    storyElement.map((element)=>(
+                        element.type==='image'?<div className='col-lg-6 col-xl-6 col-md-12 col-sm-12'><div>
+                            <img width="100%" src = {`https://gumlet.assettype.com/${element['image-s3-key']}`} alt='fashion'></img>
+                            <div className='HeldaneDisplay-Regular image-text' dangerouslySetInnerHTML={{ __html: element['title']}}></div>
+                        </div></div>:""
+                        ))
+                    } */}
+        </div>
+      </div>
+    );
+}
+
+export default EstdFullFormatDynamicCard01;
