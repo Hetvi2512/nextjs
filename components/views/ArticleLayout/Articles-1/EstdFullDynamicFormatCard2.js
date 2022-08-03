c9af9e12a736ef97698cefd069503df0cdbeb053
hetvi2512 <hetvishah2512@gmail.com>
2022-08-03 11:46:59 +0530

    Added EstdFullDynamicFormatCard2

---
+++ components/views/ArticleLayout/Articles-1/EstdFullDynamicFormatCard2.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+function EstdFullDynamicFormatCard2({ cards }) {
+  return (
+    <div>
+      <div className="black-bg black-bg-m-p row row-data ma-0  pbp-10">
+        <div className="col-lg-6 col-xl-6 col-md-12 col-sm-12 mtp-10 d-flex align-center p-0">
+          <div className="style-grid-new ffd-b-img">
+            {storyElement?.map((element) =>
+              element.subtype === "image-gallery"
+                ? element["story-elements"].map((imageData, index) => (
+                    <div key={index}>
+                      <div>
+                        <img
+                          width="100%"
+                          src={`https://gumlet.assettype.com/${imageData["image-s3-key"]}`}
+                        ></img>
+                        <div
+                          className="image-text HeldaneDisplay-Regular"
+                          dangerouslySetInnerHTML={{
+                            __html: imageData["title"],
+                          }}
+                        ></div>
+                      </div>
+                    </div>
+                  ))
+                : ""
+            )}
+          </div>
+        </div>
+        <div className="col-lg-6 col-xl-6 col-md-12 col-sm-12 mtp-10 d-flex align-center p-0">
+          {storyElement?.map((element, index) =>
+            element.type === "text" ? (
+              <div
+                key={index}
+                className="row-2-description-part pbp-5"
+                dangerouslySetInnerHTML={{ __html: element.text }}
+              ></div>
+            ) : (
+              ""
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EstdFullDynamicFormatCard2;
