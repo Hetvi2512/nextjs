386e088ae9bf6a17eab47841bd04fc8af25edc92
hetvi2512 <hetvishah2512@gmail.com>
2022-08-08 16:32:55 +0530

    Added enter function

--- components/common/Navbar/ModernDrawer.js
+++ components/common/Navbar/ModernDrawer.js
@@ -1,34 +1,58 @@
 import useSWR from "swr";
 import Image from "next/image";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 import React, { useState } from "react";
 import Drawer from "react-modern-drawer";
 import styles from "./ModernDrawer.module.css";
 import SearchIcon from "../../../public/images/Search-test.png";
 function ModernDrawer({ searchVisible, onCloseSearch }) {
   const [inputValue, setInputValue] = useState("");
-  const router = useRouter()
+
+  const router = useRouter();
   const trendFetcher = async () => {
     const response = await fetch(`/api/v1/collection/breaking-news`);
     const data = await response.json();
     return data;
   };
-  const { data : trendResponse } = useSWR(
-    "TrendData",
-    trendFetcher
-  );
+  const { data: trendResponse } = useSWR("TrendData", trendFetcher);
   const handleIndividualStory = (a) => {
-    router.push(`/${a["slug"]}`, undefined, { shallow: true })
+    router.push(`/${a["slug"]}`, undefined, { shallow: true });
   };
   const handleTrendingStory = (a) => {
-    router.push(`/${a["story"]["slug"]}`, undefined, { shallow: true })
+    router.push(`/${a["story"]["slug"]}`, undefined, { shallow: true });
   };
   const fetcher = async () => {
     const response = await fetch(`/api/v1/search/${inputValue}`);
     const data = await response.json();
     return data;
   };
-  const { data: searchResponse, error } = useSWR(inputValue.length > 0 ? inputValue : null, fetcher );
+  const { data: searchResponse, error } = useSWR(
+    inputValue.length > 0 ? inputValue : null,
+    fetcher
+  );
+  function handleEnter(e) {
+    console.log("enter", trendResponse);
+    if (e.key === "Enter") {
+      e.preventDefault(); // Ensure it is only this code that runs
+      router.push(
+        {
+          pathname: "/search",
+          query: {
+            searchResponse: JSON.stringify(searchResponse?.results?.stories),
+            trendResponse: JSON.stringify(trendResponse?.items),
+            inputValue,
+          },
+        },
+        "/search",
+        { shallow: true }
+      );
+      // props.history.push("/search", {
+      //   searchResponse: searchResponse,
+      //   trendResponse: trendResponse,
+      //   inputValue: inputValue,
+      // });
+    }
+  }
   return (
     <>
       <Drawer
@@ -46,7 +70,7 @@ function ModernDrawer({ searchVisible, onCloseSearch }) {
             <input
               style={{ borderBottom: "1px solid white" }}
               autoComplete="off"
-              //  onKeyPress={(e) => handleEnter(e)}
+              onKeyPress={(e) => handleEnter(e)}
               value={inputValue}
               onChange={(e) => setInputValue(e.target.value)}
               type="input"
@@ -58,30 +82,32 @@ function ModernDrawer({ searchVisible, onCloseSearch }) {
           </div>
         </div>
         <div className="plp-2 ptp-2 fs-30">
-                {inputValue !== ""
-                  ? searchResponse?.results?.stories.slice(0, 5).map((item,index) => (
-                      <div
-                          key={index}
-                        onClick={() => {
-                          handleIndividualStory(item);
-                        }}
-                        className="text-white search-result-div plp-5 prp-5 cursor-pointer Biotif-SemiBold pbp-1"
-                      >
-                        {item.headline}
-                      </div>
-                    ))
-                  : trendResponse?.items?.slice(0, 5).map((item,index) => (
-                      <div
-                        key={index}
-                        onClick={() => {
-                          handleTrendingStory(item);
-                        }}
-                        className="text-white search-result-div plp-5 cursor-pointer Biotif-SemiBold pbp-1"
-                      >
-                        {item.story.headline}
-                      </div>
-                    ))}
-              </div>
+          {inputValue !== ""
+            ? searchResponse?.results?.stories
+                .slice(0, 5)
+                .map((item, index) => (
+                  <div
+                    key={index}
+                    onClick={() => {
+                      handleIndividualStory(item);
+                    }}
+                    className="text-white search-result-div plp-5 prp-5 cursor-pointer Biotif-SemiBold pbp-1"
+                  >
+                    {item.headline}
+                  </div>
+                ))
+            : trendResponse?.items?.slice(0, 5).map((item, index) => (
+                <div
+                  key={index}
+                  onClick={() => {
+                    handleTrendingStory(item);
+                  }}
+                  className="text-white search-result-div plp-5 cursor-pointer Biotif-SemiBold pbp-1"
+                >
+                  {item.story.headline}
+                </div>
+              ))}
+        </div>
       </Drawer>
     </>
   );
